08c26f5c692c5a8b4dd2fa03e40e92c7f5fcb008
Skeeb32 <skeeb32@gmail.com>
2022-05-19 14:57:22 -0400

    Notes

--- Numbers.java
+++ Numbers.java
@@ -1,3 +1,11 @@
+//1. Create a java program that prints numbers 1 to 10.
+//2. Create a Jenkins job (JOB1) to compile the java program.
+//3. Copy this job to another Jenkins job (JOB2) that prints the numbers.
+//4. Create a third Jenkins job (JOB3) to print that both the above jobs have run successfully.
+//5. The numbers have to be printed every 3 minutes and 30 seconds.
+
+
+
 public class Numbers
 {
 	public static void main(String args[])
